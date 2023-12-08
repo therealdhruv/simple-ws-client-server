0fb1c663a1d5a103ee887c4a27e60441ad332b5e
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-12-08 17:39:45 +0530

    init

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
+
+const app = express();
+const port = 3000;
+
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", async (ws, req) => {
+	ws.on("message", (message) => {
+		console.log("received: %s", message);
+		ws.send(`Hello, you sent -> ${message}`);
+	});
+});
+
+app.get("/health", (req, res) => {
+	res.json({ msg: "I am healthy" });
+});
+
+server.listen(port);
